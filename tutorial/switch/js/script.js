5e7a453147c311948ce8c38af73ca2ee7b385edc
Vladlen Schetinin <born-1965@mail.ru>
2023-03-01 04:38:03 +0300

    Конструкция "switch"

--- tutorial/switch/js/script.js
+++ tutorial/switch/js/script.js
@@ -1,1 +1,110 @@
 'use strict';
+
+//  Конструкция "switch" синтаксис
+
+// switch(x) {
+//   case 'value1':  // if (x === 'value1')
+//     ...
+//     [break]
+
+//   case 'value2':  // if (x === 'value2')
+//     ...
+//     [break]
+
+//   default:
+//     ...
+//     [break]
+// }
+
+//  Переменная x проверяется на строгое равенство первому значению value1, затем второму value2 и так далее
+
+//  Если соответствие установлено – switch начинает выполняться от соответствующей директивы case и далее, до ближайшего break (или до конца switch)
+
+//  Если ни один case не совпал – выполняется (если есть) вариант default
+
+//  Пример работы
+console.log('==== Пример работы switch ====');
+
+let a = 2 + 2;
+switch(a) {
+  case 3: 
+    console.log('Маловато');
+    break;
+  case 4:
+    console.log('То, что надо!');
+    break;
+  case 5:
+    console.log('Это много');
+    break;
+  default:
+    console.log('Нет таких значений');
+}
+
+//  Пример без break
+console.log('==== Пример без break ====');
+
+let b = 2 + 5;
+switch(b) {
+  case 5:
+    console.log('Не то');
+  case 6: 
+    console.log('Опять не то');
+  case 7:
+    console.log('То, что надо');
+  case 8: 
+    console.log('Много');
+  default:
+    console.log('Ничего не могу найти');
+}
+
+//  И switch и case допускают любое выражение в качестве аргумента
+console.log('Дополнительный пример');
+
+a = "1";
+b = 0;
+switch (+a) {
+  case b + 1:
+    console.log("Выполнится, т.к. значением +a будет 1, что в точности равно b+1");
+    break;
+  default:
+    console.log("Это не выполнится");
+}
+
+//  Группировка «case»
+console.log('==== Группировка ====');
+
+a = 3;
+switch (a) {
+  case 4:
+    console.log('Правильно!');
+    break;
+  case 3: // (*) группируем оба case
+  case 5:
+    console.log('Неправильно!');
+    console.log("Может вам посетить урок математики?");
+    break;
+  default:
+    console.log('Результат выглядит странновато. Честно.');
+}
+
+//  Тип имеет значение
+console.log('== Тип имеет значение ==');
+
+let arg = prompt('Введите число');
+switch(arg) {
+  case '0':
+  case '1':
+    alert('Один или ноль');
+    break;
+  case '2': 
+    alert('Два');
+    break;
+  case 3:
+    alert('Никогда не выполнится');
+    break;
+  default:
+    alert('Неизвестное значение');
+}
+
+//  Так как promt возвращает строку, введение числа невозможно, и вызовет неизвестное значение
+//  Это произойдет из-за несовпадения типа данных
--- tutorial/switch/js/task.js
+++ tutorial/switch/js/task.js
@@ -1,2 +1,87 @@
 'use strict';
 
+//  Напишите "if", аналогичный "switch"
+
+// switch (browser) {
+//   case 'Edge':
+//     alert( "You've got the Edge!" );
+//     break;
+
+//   case 'Chrome':
+//   case 'Firefox':
+//   case 'Safari':
+//   case 'Opera':
+//     alert( 'Okay we support these browsers too' );
+//     break;
+
+//   default:
+//     alert( 'We hope that this page looks ok!' );
+// }
+
+//  Мое решение
+
+let browser = 'Chrome';
+let group = 'Okay we support these browsers too';
+
+if(browser === 'Edge') {
+  console.log( "You've got the Edge!" );
+} else if(browser === 'Chrome') {
+  console.log(group);
+} else if(browser === 'Firefox') {
+  console.log(group);
+} else if(browser === 'Safari') {
+  console.log(group);
+} else if(browser === 'Opera') {
+  console.log(group);
+} else {
+   console.log( 'We hope that this page looks ok!' );
+}
+
+//  Решение по учебнику
+
+// if(browser == 'Edge') {
+//   console.log("You've got the Edge!");
+// } else if (browser == 'Chrome'
+//  || browser == 'Firefox'
+//  || browser == 'Safari'
+//  || browser == 'Opera') {
+//   console.log( 'Okay we support these browsers too' );
+// } else {
+//   console.log( 'We hope that this page looks ok!' );
+// }
+
+//  Конструкция browser == 'Chrome' || browser == 'Firefox' ... разбита на несколько строк для лучшей читаемости
+//  Запись через switch нагляднее
+
+//  Переписать условия "if" на "switch"
+
+// const number = +prompt('Введите число между 0 и 3', '');
+// if (number === 0) {
+//   alert('Вы ввели число 0');
+// }
+// if (number === 1) {
+//   alert('Вы ввели число 1');
+// }
+// if (number === 2 || number === 3) {
+//   alert('Вы ввели число 2, а может и 3');
+// }
+
+const number = +prompt('Введите число между 0 и 3', '');
+//  Внимание: prompt в любом случае преобразует значение в строку, а вот + это значение уже преобразует в числовой тип
+switch(number) {
+  case 0:
+    alert('Вы ввели число 0');
+    break;
+  case 1:
+    alert('Вы ввели число 1');
+    break;
+  case 2:
+  case 3:
+    alert('Вы ввели число 2, а может и 3');
+    break;
+  default:
+    alert('Неизвестное значение');
+}
+
+//  Внимание: break внизу не обязателен, но ставится по «правилам хорошего тона»
+//  Допустим, он не стоит. Есть шанс, что в будущем нам понадобится добавить в конец ещё один case, например case 4, и мы, вполне вероятно, забудем этот break поставить. В результате выполнение case 2/case 3 продолжится на case 4 и будет ошибка
